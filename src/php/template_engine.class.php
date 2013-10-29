52994d2208de06c0cb004bad587e035f08d60c2a
joschi03 <janosch.rohdewald@gmail.com>
2013-10-29 20:26:04 +0100

    initial php project creation

---
+++ src/php/template_engine.class.php
@@ -0,0 +1,38 @@
+<?php
+require_once("config.class.php");
+
+class TemplateEngine
+{
+    var $config;
+    var $TEMPLATE_VARS = array();
+
+    function TemplateEngine()
+    {
+        $this->config = new config();
+    }
+
+    function getconfig()
+    {
+        $this->config = new config();
+    }
+
+    function assign($key, $val=null)
+    {
+        $this->TEMPLATE_VARS[$key] = $val;
+    }
+
+    function display($tpl_file)
+    {
+        if(!is_file($this->config->tplDir.$tpl_file.".tpl.php")) {
+            exit('no such template file: '.$this->config->tplDir.$tpl_file.".tpl.php");
+        }
+
+        # Kurze Referenz auf die Template-Variable
+        $TPL = &$this->TEMPLATE_VARS;
+
+        # Variabeln entpacken, damit $this weggelassen werden kann
+        extract(get_object_vars($this));
+
+        include($config->tplDir.$tpl_file.".tpl.php");
+    }
+}
