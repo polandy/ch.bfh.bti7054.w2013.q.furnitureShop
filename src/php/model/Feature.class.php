3f30e3a917e1527b8fbb218256fa7e820b0e1494
andy <polla2@bfh.ch>
2014-01-12 17:17:04 +0100

    feature object

--- src/php/model/Feature.class.php
+++ src/php/model/Feature.class.php
@@ -3,4 +3,51 @@
 namespace model;
 
 class Feature extends generic\GenericNamedModel {
+
+    public $extraPrice;
+    public $furnitureId;
+
+    function __construct($extraPrice = null, $furnitureId = null, $name_de = NULL, $name_en = NULL)
+    {
+        $this->name_de = $name_de;
+        $this->name_en = $name_en;
+        $this->extraPrice = $extraPrice;
+        $this->furnitureId = $furnitureId;
+    }
+
+
+    /**
+     * @param mixed $extraPrice
+     */
+    public function setExtraPrice($extraPrice)
+    {
+        $this->extraPrice = $extraPrice;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getExtraPrice()
+    {
+        return $this->extraPrice;
+    }
+
+    /**
+     * @param mixed $furnitureId
+     */
+    public function setFurnitureId($furnitureId)
+    {
+        $this->furnitureId = $furnitureId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFurnitureId()
+    {
+        return $this->furnitureId;
+    }
+
+
+
 }
