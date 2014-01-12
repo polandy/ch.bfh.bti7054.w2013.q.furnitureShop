6f76a88bc3111073689edd1781bfd300f8f5c622
andy <polla2@bfh.ch>
2014-01-12 14:53:11 +0100

    admin menu in footer

--- src/tmpl/footer.tpl.php
+++ src/tmpl/footer.tpl.php
@@ -1,16 +1,18 @@
 <footer class="row">
     <div class="large-12 columns">
-        <hr />
+        <hr/>
         <div class="row">
             <div class="large-6 columns">
                 <p>&copy; 2013–2013 M&ouml;bius Furniturus, Inc. All rights reserved.</p>
             </div>
             <div class="large-6 columns">
                 <ul class="inline-list right">
-                    <li><a href="#">Section 1</a></li>
-                    <li><a href="#">Section 2</a></li>
-                    <li><a href="#">Section 3</a></li>
-                    <li><a href="#">Section 4</a></li>
+                    <?php
+                    if (Config::getInstance()->isAdmin()) { ?>
+                        <li><a href="?pageId=100"><?=$TPL['createCategory']?></a></li>
+                        <li><a href="?pageId=200"><?=$TPL['createFurniture']?></a></li>
+
+                    <?php } // end if ?>
                 </ul>
             </div>
         </div>
