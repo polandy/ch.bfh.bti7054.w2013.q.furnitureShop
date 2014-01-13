d92e6d06cb1258a368936d70bf0cd03d66b7d466
andy <polla2@bfh.ch>
2014-01-13 16:43:12 +0100

    footer

--- src/tmpl/footer.tpl.php
+++ src/tmpl/footer.tpl.php
@@ -7,22 +7,26 @@
     <div class="large-12 columns">
         <hr/>
         <div class="row">
-            <div class="large-5 columns">
-                <p>&copy; 2013–2013 M&ouml;bius Furniturus, Inc. All rights reserved.</p>
+            <div class="large-4 columns">
+                <h6 id="footerText" class="subheader small"> &copy; 2013–2013 M&ouml;bius Furniturus, Inc. All rights reserved.</h6>
             </div>
             <div class="large-6 columns">
                 <ul class="inline-list right">
                     <?php
-                    if (Config::getInstance()->isAdmin()) { ?>
-                        <li><a href="?pageId=100"><?=$TPL['createCategory']?></a></li>
-                        <li><a href="?pageId=200"><?=$TPL['createFurniture']?></a></li>
+                    if (Config::getInstance()->isAdmin()) {
+                        ?>
+                        <li><a href="?pageId=100"><?= $TPL['createCategory'] ?></a></li>
+                        <li><a href="?pageId=200"><?= $TPL['createFurniture'] ?></a></li>
                     <?php } // end if ?>
-                        <li><a href="?pageId=4"><?=$TPL['impressum']?></a></li>
+                    <li><a href="?pageId=4"><?= $TPL['impressum'] ?></a></li>
                 </ul>
             </div>
-            <div class="large-1 columns">
-                <a href='<?=$TPL["langURL"]?>de'>de</a> /
-                <a href='<?=$TPL["langURL"]?>en'>en</a>
+            <div class="small-2 columns">
+                <ul class="inline-list right">
+                    <li><a href='<?= $TPL["langURL"] ?>de'>de</a></li>
+                    <li>/</li>
+                    <li><a href='<?= $TPL["langURL"] ?>en'>en</a></li>
+                </ul>
             </div>
         </div>
     </div>
